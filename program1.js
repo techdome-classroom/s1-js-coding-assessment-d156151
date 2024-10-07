6f3a86b3ab537fa6e36bdf86bc078bab90d189d1
d156151 <dc864645@gmail.com>
2024-10-07 12:59:14 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -1,7 +1,37 @@
 const getTotalIsles = function (grid) {
 
 
-  // write your code here
+  // write your code herefunction numIslands(grid){
+
+if(grid.length===0||
+   grid[0].length===0)return 0;
+  
+  const rows =grid.length;
+  const cols =grid[0].length;
+  let islandCount =0;
+  function dfs(grid,i,j){
+if(i<0||j<0||i>=rows
+   || j>=cols||grid[i][j]==='w')
+{
+return;
+  }
+grid[i][j]='w';
+    dfs(grid,i-1,j);
+        dfs(grid,i+1,j);
+        dfs(grid,i,j-1);
+        dfs(grid,i,j+1);
+  }
+  for(let i=0;i<rows;i++){
+     for(let j=0;j<cols;j++){
+       if(grid[i][j]==='L'){
+         islandCount++;
+         dfs(grid,1,j);
+  }
+}
+}
+  return islandCount;
+}
+
 
 };
 
